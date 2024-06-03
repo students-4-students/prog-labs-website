d5a3e979295caca21d2821b32e1ecf83f3fdffbc
Mw3y <maxence1604.minecraft@gmail.com>
2024-06-03 15:33:34 +0200

    ➕ Add shadcn ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt'],
+  modules: ['@pinia/nuxt', 'shadcn-nuxt', '@nuxtjs/tailwindcss'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui',
+  },
 });
