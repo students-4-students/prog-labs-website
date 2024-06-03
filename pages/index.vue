1d623d1813e71e19ac2106b159332d6fb755cee8
Mw3y <maxence1604.minecraft@gmail.com>
2024-06-03 15:00:48 +0200

    ✨ Add routing

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+	const route = useRoute();
+</script>
+
+<template>
+	<div>
+		<!-- https://nuxt.com/docs/getting-started/routing -->
+		<NuxtWelcome />
+	</div>
+</template>
