e481f183b71b25e2dd77edc8ac27f43f3fcaf6fd
Eliott Tornassat <eliott.tornassat@epfl.ch>
2024-07-22 12:22:03 +0200

    fixed no found div not being vertically centered

--- components/playground/ExerciseView.vue
+++ components/playground/ExerciseView.vue
@@ -7,14 +7,16 @@
 </script>
 
 <template>
-  <div class="flex h-full items-center justify-center bg-accent">
-    <ScrollArea class="h-full">
-      <ContentRenderer :value="props.exerciseData">
+  <div class="h-full bg-accent">
+    <ContentRenderer :value="props.exerciseData">
+      <ScrollArea class="h-full">
         <ContentRendererMarkdown
           :value="props.exerciseData"
           class="p-4 prose font-main text-justify prose-headings:text-left dark:prose-invert text-neutral-700 dark:text-neutral-200 prose-img:w-full prose-img:border prose-img:rounded-md prose-img:first:mt-0 prose-pre:bg-background prose-pre:border prose-pre:p-4 lg:prose-lg prose-code:font-code prose-code:after:content-[''] prose-code:before:content-[''] prose-h2:after:mt-2 prose-h2:font-[750] prose-h2:first:mt-0 prose-h2:after:content-[''] prose-h2:after:w-full prose-h2:after:h-0.5 prose-h2:after:block prose-h2:after:bg-primary/50 prose-h3:after:mt-1 prose-h3:after:content-[''] prose-h3:after:w-full prose-h3:after:h-0.5 prose-h3:after:block prose-h3:after:bg-primary/35 prose-a:prose-headings:font-[610] prose-a:prose-headings:pointer-events-none prose-a:prose-headings:cursor-default prose-a:prose-headings:no-underline prose-a:prose-headings:text-neutral-950 prose-a:prose-headings:dark:text-neutral-100 prose-a:font-[570] prose-a:underline-offset-[3px] prose-a:text-primary prose-strong:font-semibold prose-strong:dark:font-bold prose-strong:dark:text-[0.99em] prose-code:prose-p:bg-background prose-code:prose-p:dark:border-slate-400/25 prose-code:prose-p:break-keep prose-code:prose-p:border prose-code:prose-p:rounded prose-code:prose-p:px-2 prose-code:prose-p:m-0.5 prose-code:prose-p:inline-block prose-blockquote:border-slate-300/50 prose-ul:mt-0"
         />
-        <template #empty>
+      </ScrollArea>
+      <template #empty>
+        <div class="flex h-full items-center justify-center">
           <div class="flex flex-col space-y-4 items-center justify-center">
             <NuxtImg
               class="mb-4"
@@ -28,8 +30,8 @@
               Choisir un autre exercice
             </Button>
           </div>
-        </template>
-      </ContentRenderer>
-    </ScrollArea>
+        </div>
+      </template>
+    </ContentRenderer>
   </div>
 </template>
