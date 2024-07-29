47d9ed92e3b8b008c4a9258675404dd428e6aa57
Mw3y <maxence1604.minecraft@gmail.com>
2024-07-29 19:08:46 +0200

    ✨ Add python language server

---
+++ components/playground/editor/language-servers/index.ts
@@ -0,0 +1,103 @@
+import type { UserConfig } from 'monaco-editor-wrapper'
+import type { MessageTransports } from 'vscode-languageclient'
+import { CloseAction, ErrorAction } from 'vscode-languageclient'
+import { Uri } from 'vscode'
+
+export abstract class LanguageServer {
+  private _languageId: string
+  private _worker!: Worker
+  private _transports!: MessageTransports
+  private _initializationOptions?: object
+
+  protected constructor({
+    languageId,
+    worker,
+    transports,
+    initializationOptions,
+  }: {
+    languageId: string
+    worker: Worker
+    transports: MessageTransports
+    initializationOptions?: object
+  }) {
+    this._languageId = languageId
+    this._worker = worker
+    this._transports = transports
+    this._initializationOptions = initializationOptions
+  }
+
+  /**
+   * Waits for the first message of a worker.
+   * @param worker the worker to wait for
+   * @param failAfterMs the maximum time to wait for
+   * @returns a promise which resolves on the first worker message
+   */
+  static async awaitWorkerLoad(worker: Worker, failAfterMs = 8000) {
+    let markAsReady = (_: unknown) => {}
+    let markAsFailed = () => {}
+
+    const timeout = setTimeout(() => markAsFailed(), failAfterMs)
+    worker.addEventListener('message', (message) => {
+      if (message.data.type === 'ready') {
+        clearTimeout(timeout)
+        markAsReady(true)
+      }
+    })
+
+    return new Promise((resolve, reject) => {
+      markAsReady = resolve
+      markAsFailed = reject
+    })
+  }
+
+  /**
+   * Creates the language client config for the monaco editor wrapper.
+   * @returns the language client config
+   */
+  createMonacoConfig(): UserConfig['languageClientConfig'] {
+    return {
+      name: `${this.languageId}-language-client`,
+      languageId: this.languageId,
+      options: {
+        $type: 'WorkerDirect',
+        worker: this.worker,
+      },
+      clientOptions: {
+        documentSelector: [this.languageId],
+        initializationOptions: this.initializationOptions,
+        errorHandler: {
+          error: () => ({ action: ErrorAction.Continue }),
+          closed: () => ({ action: CloseAction.DoNotRestart }),
+        },
+        workspaceFolder: {
+          index: 0,
+          name: 'workspace',
+          uri: Uri.file('workspace'),
+        },
+      },
+      connectionProvider: {
+        get: async () => this.transports,
+      },
+    }
+  }
+
+  dispose() {
+    this._worker.terminate()
+  }
+
+  get languageId() {
+    return this._languageId
+  }
+
+  get worker() {
+    return this._worker
+  }
+
+  get transports() {
+    return this._transports
+  }
+
+  get initializationOptions() {
+    return this._initializationOptions
+  }
+}
---
+++ components/playground/editor/language-servers/python/loadTypeDefinitions.ts
@@ -0,0 +1,38 @@
+import { loadAsync, type JSZipObject } from 'jszip'
+import typeDefinitionsUrl from '@/assets/python/stdlib-source-with-typeshed-pyi.zip?url'
+
+const noTrailingSlash = ([name]: [string, JSZipObject]) => !name.endsWith('/')
+
+/**
+ * Reads content of a zip file using jszip.
+ * @param url url of the zip file
+ * @returns object with file path and file contents
+ */
+async function readZipFile(
+  url: string,
+  sanitizeFileName: (filename: string) => string,
+) {
+  try {
+    const response = await fetch(url)
+    const data = await response.arrayBuffer()
+    const results: { [id: string]: string } = {}
+    const zip = await loadAsync(data)
+    const files = Object.entries(zip.files)
+    for (const [filename, file] of files.filter(noTrailingSlash))
+      results[sanitizeFileName(filename)] = await file.async('text')
+    return results
+  } catch (error) {
+    console.error(error)
+    return {}
+  }
+}
+
+/**
+ * Loads the python type definitions files from the server.
+ * @returns the python type definitions files
+ */
+export async function loadTypeDefinitionFiles() {
+  const tryPrependSlash = (filename: string) =>
+    filename.replace(/^(stdlib|stubs)/, '/$1')
+  return await readZipFile(typeDefinitionsUrl, tryPrependSlash)
+}
