3a434867e96c6605c276fa64eecc248b388f1d60
Max <maxence1604.minecraft@gmail.com>
2024-08-05 16:52:36 +0200

    🚧 WIP 'about' page

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex px-4 md:px-8 py-8 w-full h-full gap-8">
+    <div class="flex-1">
+      <div class="flex flex-col gap-3">
+        <h2 className="flex text-4xl font-bold tracking-normal">
+          Quelle est l'<span class="text-primary underline">équipe</span>
+          &nbsp;derrière ce site ?
+        </h2>
+        <p class="text-xl text-foreground/85">
+          Nisi veniam consectetur adipisicing velit. Sit culpa labore labore
+          aliquip deserunt eu esse amet. Irure nostrud nostrud tempor nostrud
+          eiusmod aliquip.
+        </p>
+      </div>
+    </div>
+    <div class="flex-1 bg-green-500 rounded-xl border shadow-lg"></div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
