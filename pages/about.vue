acf810405aa90b6e7bf2c8b68ef1902ed3ee7ada
Max <maxence1604.minecraft@gmail.com>
2024-08-05 19:01:11 +0200

    🍱 Add image and information display on about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,22 +1,31 @@
 <template>
-  <div class="flex px-4 md:px-8 py-8 w-full h-full gap-8">
-    <div class="flex-1">
+  <div class="flex flex-col 2xl:flex-row px-4 md:px-8 py-8 w-full h-full gap-4">
+    <div class="flex flex-col basis-7/12 gap-y-8">
       <div class="flex flex-col gap-3">
-        <h2 className="flex text-4xl font-bold tracking-normal">
+        <h2 className="text-4xl font-bold tracking-normal">
           Quelle est l'<span class="text-primary underline">équipe</span>
-          &nbsp;derrière ce site ?
+          derrière ce site web ?
         </h2>
-        <p class="text-xl text-foreground/85">
+        <p class="text-2xl text-foreground/85">
           Nisi veniam consectetur adipisicing velit. Sit culpa labore labore
           aliquip deserunt eu esse amet. Irure nostrud nostrud tempor nostrud
           eiusmod aliquip.
         </p>
       </div>
+      <div class="flex flex-wrap gap-16">
+        <AboutInfo title="Développeur">
+          <template #icon>
+            <LucideLaptop />
+          </template>
+          Lorem Ipsum
+        </AboutInfo>
+      </div>
     </div>
-    <div class="flex-1 bg-green-500 rounded-xl border shadow-lg"></div>
+    <div class="flex basis-5/12">
+      <NuxtImg
+        class="h-full w-full border shadow-lg object-cover rounded-xl"
+        src="/illustrations/team-photo-placeholder.png"
+      />
+    </div>
   </div>
 </template>
-
-<script lang="ts" setup></script>
-
-<style></style>
