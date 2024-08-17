d868fc7f9af6d0ed1f97a424f4ca5c80446ad523
Max <maxence1604.minecraft@gmail.com>
2024-08-17 19:03:05 +0200

    🐛 Add aspect ratio to about images to prevent layout shifting

--- components/about/AboutInfo.vue
+++ components/about/AboutInfo.vue
@@ -8,7 +8,7 @@
 <template>
   <div class="flex flex-col space-y-1 w-28">
     <NuxtImg
-      class="border rounded-lg"
+      class="border rounded-lg aspect-square"
       :src="props.photoUrl ?? '/person-placeholder.png'"
     />
     <p class="text-lg font-semibold text-foreground max-full">
